600b94e0cc4e0c269475cc1988c8e6038b02f68f
jihye-jane <76241170+jihye-jane@users.noreply.github.com>
2025-12-17 04:50:42 +0900

    Delete input directory

--- input/__init__.py
+++
@@ -1,8 +0,0 @@
-# input/__init__.py
-
-from .lidar_sensor import LidarSensor
-from .depth_camera_sensor import DepthCameraSensor
-from .gps_imu_sensor import GpsImuSensor
-from .manual_input import ManualInput
-
-print("Input package loaded.")
